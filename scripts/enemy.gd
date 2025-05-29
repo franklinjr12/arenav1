d5120c50a2de722651a0a2a481e58afe13c89ba4
Franklin Junior <franklinjunior12@hotmail.com>
2025-05-29 09:31:08 -0300

    enemy chasing player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,18 +5,37 @@ class_name Enemy
 
 var health_points = 5
 var attack_range = 30
+var should_chase = false
+@export var chase_range = 300
+@export var speed_multiplier = 2500
 
 func _process(delta: float):
 	var player: Player = get_player()
 	if player != null:
 		var distance = player.position - position
-		if distance.length() < attack_range && can_attack():
+		var distance_length = distance.length()
+		if distance_length < attack_range && can_attack():
 			var attack = melee_spell.instantiate()
 			attack.position = position + distance
 			# TODO should check if is in arena
 			get_tree().get_first_node_in_group("Game").add_child(attack)
 			$AttackTimer.start()
-			print("attacking")
+		if distance_length < chase_range:
+			should_chase = true
+		else:
+			should_chase = false
+
+
+func _physics_process(delta: float) -> void:
+	if should_chase:
+		var player: Player = get_player()
+		if player != null:
+			var distance = player.position - position
+			velocity = distance.normalized() * speed_multiplier * delta
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
+
 
 func suffer_damage(number: int):
 	health_points -= number
