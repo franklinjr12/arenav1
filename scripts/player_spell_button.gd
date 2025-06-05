d45dc2fc9d3697b7d2781d3ce17e304cd84d0a2b
Franklin Junior <franklinjunior12@hotmail.com>
2025-06-05 09:54:45 -0300

    spell cooldown gradiant

--- scripts/player_spell_button.gd
+++ scripts/player_spell_button.gd
@@ -1,10 +1,26 @@
 extends TextureButton
 
+const cooldown_color: Color = Color("ff0000ab")
+const color_transparency: float = cooldown_color.a
+
+
 func _ready():
 	$Timer.timeout.connect(_on_timer_timeout)
+	$ColorRect.size = size
+	$ColorRect.color = cooldown_color
+	$ColorRect.visible = false
+
+
+func _process(_delta: float):
+	if !$Timer.is_stopped():
+		var current_time: float = $Timer.time_left
+		var wait_time: float = $Timer.wait_time
+		var transparency: float = color_transparency * (current_time / wait_time)
+		$ColorRect.color.a = transparency
 
 
 func start_cooldown():
+	$ColorRect.color.a = color_transparency
 	$ColorRect.visible = true
 	$Timer.start()
 
